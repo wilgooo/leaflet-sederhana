77f08718064e7f628fc6f84d640f7cdb8a8aaeab
William Gozali <147629271+wilgooo@users.noreply.github.com>
2024-07-10 02:34:46 +0700

    leaflet-sederhana

---
+++ data/Universitas_5.js
@@ -0,0 +1,68 @@
+var json_Universitas_5 = {
+    "type": "FeatureCollection",
+    "name": "Universitas_5",
+    "crs": {
+      "type": "name",
+      "properties": {
+        "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+      }
+    },
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "Fasilitas": "Universitas 1",
+          "ImageURL": "https://awsimages.detik.net.id/community/media/visual/2019/02/12/43b5e043-b813-47c9-b0c1-3c8fa14bf6ae_169.jpeg?w=600&q=90"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [112.306157783559982, -7.516617299640692]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Fasilitas": "Universitas 2",
+          "ImageURL": "https://asset.kompas.com/crops/2ChUs-Oas2KT98-3D7SlJP3SvIg=/15x99:974x578/750x500/data/photo/2018/06/21/3039337330.jpg"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [112.268332943204513, -7.555349477277124]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Fasilitas": "Universitas 3",
+          "ImageURL": "https://maukuliah.ap-south-1.linodeobjects.com/gallery/001031/Gedung%201%20UT%20Tangerang-thumbnail.jpg"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [112.226589210546521, -7.537149109693056]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Fasilitas": "Universitas 4",
+          "ImageURL": "https://kaltimkece.kedeka.sg-sin1.upcloudobjects.com/media/01gy2ave0xajdwscckcgmbmqt3/conversions/mengenal-lebih-dalam-universitas-terbuka-kampus-terbesar-kesembilan-di-dunia-800.jpg"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [112.286366739767928, -7.477773863827747]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Fasilitas": "Universitas 5",
+          "ImageURL": "https://pict.sindonews.net/dyn/850/pena/news/2021/11/30/211/614747/berhektarehektare-inilah-5-universitas-terluas-di-indonesia-mov.jpg"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [112.207464529893002, -7.468741920851945]
+        }
+      }
+    ]
+  }
+  
